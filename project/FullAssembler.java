603c2a190f4f3b0ba8b45b4fbb2085a14b8aaae3
Henry Burns <hhburnsboy@gmail.com>
2018-05-07 13:54:05 -0400

    fixed problems in FullAssembler

--- project/FullAssembler.java
+++ project/FullAssembler.java
@@ -22,47 +22,75 @@ public class FullAssembler implements Assembler {
                 String temp = scan.nextLine();
                 codeLine++;
                 if(hasBlank) {
-                    if (temp.trim().length() > 0)
-                        codeAgain = true;
-                    if(codeAgain) {
-                        if(!blankError)
-                            error.append("\nIllegal blank line in the source file on line " + blankline);
-                        blankError = true;
+                    if (temp.trim().length() > 0) {
+                        error.append("\nIllegal blank line in the source file on line " + blankline);
+                        hasBlank = false;
+                        retValue = blankline;
                     }
                 }
-                if (temp.trim().length() == 0) {
+                if(temp.trim().length() == 0){
+                    blankline = codeLine;
                     hasBlank = true;
-                    if(!hasBlank)
-                        blankline = codeLine;
                     continue;
-                    } else if (temp.substring(0, 1).equals(' ') || temp.substring(0, 1).equals('\t')) {
+                }
+                else if (temp.substring(0, 1).equals(' ') || temp.substring(0, 1).equals('\t')) {
                     error.append("\nLine starts with illegal white space");
                     retValue = codeLine;
                 }
-                if (temp.equalsIgnoreCase("DATA")) {
-                    if (!temp.equals("DATA")) {
+                if (pastData) {
+                    parts = temp.trim().split("\\s+");
+                    if (parts.length < 2) {
+                        error.append("\nError on line " + codeLine + ": this data is missing an argument");
+                        retValue = codeLine;
+                    }
+                    else {
+                        try {
+                            System.out.println("PART 1: " + parts[1]);
+                            value = Integer.parseInt(parts[1], 16);
+                        } catch (NumberFormatException e) {
+                            error.append("\nError on line " + codeLine +
+                                    ":  non-numeric value");
+                            retValue = codeLine;
+                        }
+                        try {
+                            System.out.println("PART 0: " + parts[0]);
+                            address = Integer.parseInt(parts[0], 16);
+                        } catch (NumberFormatException e) {
+                            error.append("\nError on line " + codeLine +
+                                    ":  non-numeric memory address");
+                            retValue = codeLine;
+                        }
+                    }
+                }
+
+                if (temp.trim().equalsIgnoreCase("DATA")) {
+                    if (!temp.trim().equals("DATA")) {
                         error.append("\nLine does not have DATA in upper case");
                         retValue = codeLine;
                     }
                     pastData = true;
                     continue;
-                } else if (pastData) {
-                    parts = temp.split("\\s");
-
-                        value = Integer.parseInt(parts[1], 16);
-                        address = Integer.parseInt(parts[0],16);
-                    }
+                }
                 if(!pastData){
-                    parts = temp.split("\\s");
+                    parts = temp.trim().split("\\s+");
                     if (!InstrMap.toCode.keySet().contains(parts[0])) {
                         error.append("\nError on line " + (codeLine) + ": illegal mnemonic");
                         retValue = codeLine;
                     } else {
-                        if (parts.length > 1)
+                        if(parts.length < 1)
                             if (noArgument.contains(parts[0])) {
                                 error.append("\nError on line " + codeLine + ": this mnemonic cannot take arguments");
                                 retValue = codeLine;
-                            } else {
+                            }
+                            else {
+                                try {
+                                    System.out.println("PART 1: " + parts[1]);
+                                    value = Integer.parseInt(parts[1], 16);
+                                } catch (NumberFormatException e) {
+                                    error.append("\nError on line " + codeLine +
+                                            ":  non-numeric value");
+                                    retValue = codeLine;
+                                }
                                 if (parts.length > 2) {
                                     error.append("\nError on line " + codeLine + ": this mnemonic has too many arguments");
                                     retValue = codeLine;
@@ -72,6 +100,7 @@ public class FullAssembler implements Assembler {
                                     error.append("\nError on line " + codeLine + ": this mnemonic is missing an argument");
                                     retValue = codeLine;
                                 }
+
                             }
                         if (!parts[0].toUpperCase().equals(parts[0])) {
                             error.append("\nError on line " + codeLine + ": mnemonic must be upper case");
@@ -87,10 +116,6 @@ public class FullAssembler implements Assembler {
         } catch (IOException e){
             error.append("\nUnexplained IO Exception");
             return -1;
-        }catch (NumberFormatException e) {
-            error.append("\nError on line " + codeLine +
-                    ": argument is not a hex number");
-            return codeLine;
         }
         if(retValue == 0) {
             SimpleAssembler assembler = new SimpleAssembler();
